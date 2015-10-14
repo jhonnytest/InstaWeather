8ab973a95fb2cb7bae61383747f9017a9a01fb1d
Jhonny Test <iriyaz93@gmail.com>
2015-10-14 20:34:46 +0530

    Initial Commit

---
+++ InstaWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaWeather
+//
+//  Created by Riyaz Ahamed on 14/10/15.
+//  Copyright (c) 2015 SimplyApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
